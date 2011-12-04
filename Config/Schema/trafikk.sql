2ef39d0f7092fcfb95aa76387671a98399911218
bovan <bovan@bovan.org>
2011-12-04 22:59:04 +0100

    added schema

---
+++ Config/Schema/trafikk.sql
@@ -0,0 +1,37 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `heading` varchar(255) NOT NULL,
+  `messageType` int(11) NOT NULL,
+  `ingress` text NOT NULL,
+  `messagenumber` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `roadType` varchar(8) NOT NULL,
+  `roadNumber` int(11) NOT NULL,
+  `validFrom` datetime NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `endlatitude` float DEFAULT NULL,
+  `endlongitude` float DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8446 ;
+
+CREATE TABLE IF NOT EXISTS `updates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `success` tinyint(4) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
