433f26b623ac1d6f81c33483353246d238026eca
bovan <bovan@bovan.org>
2011-12-04 17:37:17 +0100

    moved logic to model

--- Controller/MessagesController.php
+++ Controller/MessagesController.php
@@ -56,30 +56,8 @@ class MessagesController extends AppController {
         
         $this->setTitle('Updating');
         
-        App::uses('Xml', 'Utility');
-
-        // get XML
-        $url = 'http://www.vegvesen.no/trafikk/xml/search.xml?searchFocus.counties=2&searchFocus.counties=9&searchFocus.counties=6&searchFocus.counties=20&searchFocus.counties=4&searchFocus.counties=12&searchFocus.counties=15&searchFocus.counties=17&searchFocus.counties=18&searchFocus.counties=5&searchFocus.counties=3&searchFocus.counties=11&searchFocus.counties=14&searchFocus.counties=16&searchFocus.counties=8&searchFocus.counties=19&searchFocus.counties=10&searchFocus.counties=7&searchFocus.counties=1&searchFocus.messageType=17&searchFocus.messageType=19&searchFocus.messageType=20&searchFocus.messageType=18&searchFocus.messageType=22&searchFocus.messageType=23&searchFocus.sortOrder=0';
-        $xml = Xml::build($url);
-
-        // XML = simpleXML object (may look odd, but it's a silly XML!)
-        $messages = $xml->{'result-array'}->result->messages;
+        $result = $this->Message->updateData();
         
-        // if we got tons of messages, wipe the db
-        // todo: could probably replace this with truncate
-        if (count($messages->message) > 0) {
-            $this->Message->deleteAll(array(true => true));
-        }
-
-        // add messages
-        $result = array('success' => true);
-        foreach ($messages->message as $message) {
-            $success = $this->Message->addMessage($message);
-            if (!$success) {
-                $result['success'] = false;
-                // TODO: add warning or stuff here
-            }
-        }
         $this->set('json', $result);
         $this->layout = 'ajax';
         $this->render('/Elements/json');
--- Model/Message.php
+++ Model/Message.php
@@ -92,4 +92,34 @@ class Message extends AppModel {
             $km = $diff * 60 * 1.8531596;
             return intval($km);
         }
+        
+        /**
+         * gets new messages
+         */
+        public function updateData() {
+            App::uses('Xml', 'Utility');
+            // get XML
+            $url = 'http://www.vegvesen.no/trafikk/xml/search.xml?searchFocus.counties=2&searchFocus.counties=9&searchFocus.counties=6&searchFocus.counties=20&searchFocus.counties=4&searchFocus.counties=12&searchFocus.counties=15&searchFocus.counties=17&searchFocus.counties=18&searchFocus.counties=5&searchFocus.counties=3&searchFocus.counties=11&searchFocus.counties=14&searchFocus.counties=16&searchFocus.counties=8&searchFocus.counties=19&searchFocus.counties=10&searchFocus.counties=7&searchFocus.counties=1&searchFocus.messageType=17&searchFocus.messageType=19&searchFocus.messageType=20&searchFocus.messageType=18&searchFocus.messageType=22&searchFocus.messageType=23&searchFocus.sortOrder=0';
+            $xml = Xml::build($url);
+
+            // XML = simpleXML object (may look odd, but it's a silly XML!)
+            $messages = $xml->{'result-array'}->result->messages;
+
+            // if we got tons of messages, wipe the db
+            // todo: could probably replace this with truncate
+            if (count($messages->message) > 0) {
+                $this->deleteAll(array(true => true));
+            }
+
+            // add messages
+            $result = array('success' => true);
+            foreach ($messages->message as $message) {
+                $success = $this->addMessage($message);
+                if (!$success) {
+                    $result['success'] = false;
+                    // TODO: add warning or stuff here
+                }
+            }
+            return $result;
+        }
 }
